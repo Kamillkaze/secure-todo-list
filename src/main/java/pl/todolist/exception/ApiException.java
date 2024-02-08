7abe8c34c2dd17f52e538d53df0fecd1d0bf957e
Kamil <kamilttt271@gmail.com>
2024-02-08 10:18:06 +0100

    Use @RequiredArgsConstructor from lombok

--- src/main/java/pl/todolist/exception/ApiException.java
+++ src/main/java/pl/todolist/exception/ApiException.java
@@ -1,16 +1,13 @@
 package pl.todolist.exception;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 
 @Getter
+@RequiredArgsConstructor
 public class ApiException {
     private final String message;
     private final HttpStatus status;
-
-    public ApiException(String message, HttpStatus status) {
-        this.message = message;
-        this.status = status;
-    }
 }
