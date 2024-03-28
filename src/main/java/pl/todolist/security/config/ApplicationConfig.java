6efdcd3443c636fcc9e568c85dbbb9811a5fbd01
Kamil <kamilttt271@gmail.com>
2024-03-28 08:03:01 +0100

    Add ApplicationConfig class with UserDetailsService bean

---
+++ src/main/java/pl/todolist/security/config/ApplicationConfig.java
@@ -0,0 +1,21 @@
+package pl.todolist.security.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import pl.todolist.security.user.UserRepository;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
