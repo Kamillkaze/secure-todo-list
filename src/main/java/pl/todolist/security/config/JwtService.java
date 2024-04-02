5ee9a52b94f73d6e7e4db6dfb83a142b2bcbc6c2
Kamil <kamilttt271@gmail.com>
2024-04-02 19:37:17 +0200

    Self review fixes

--- src/main/java/pl/todolist/security/config/JwtService.java
+++ src/main/java/pl/todolist/security/config/JwtService.java
@@ -63,7 +63,7 @@ public class JwtService {
                 .parserBuilder()
                 .setSigningKey(getSigningKey())
                 .build()
-                .parseClaimsJwt(token)
+                .parseClaimsJws(token)
                 .getBody();
     }
 
